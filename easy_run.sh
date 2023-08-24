693e9d2851a8ff686ad88758f2aeeeb3a362cde2
damiraminov1 <damiraminov1@yandex.ru>
2023-08-24 05:17:54 +0500

    easy_run.sh script fixes

--- easy_run.sh
+++ easy_run.sh
@@ -2,9 +2,9 @@ sudo apt-get install -y libgirepository1.0-dev build-essential \
   libbz2-dev libreadline-dev libssl-dev zlib1g-dev libsqlite3-dev wget \
   curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libcairo2-dev \
   libffi-dev python3.11-dev
-sudo apt install python3.11-venv
+sudo apt install -y python3.11-venv
 python3.11 -m venv venv
 source venv/bin/activate
-pip install poetry == 1.5.0
+pip install poetry==1.5.0
 poetry install
 poetry run python run
